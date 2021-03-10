21037159855f5604a9afef49eedde844186f540b
Mario De Los Santos <mario.jdls@gmail.com>
2021-03-10 09:53:51 -0500

    March102020

--- main.js
+++ main.js
@@ -354,3 +354,34 @@ class Person {
     return `${this.name}s age is ${this.age}`
   }
 }
+// Imagine you are creating a game where the user has to guess the correct number. But there is a limit of how many guesses the user can do.
+
+// If the user tries to guess more than the limit, the function should throw an error.
+// If the user guess is right it should return true.
+// If the user guess is wrong it should return false and lose a life.
+
+// Can you finish the game so all the rules are met?
+
+class Guesser {
+  constructor(number, lives) {
+    this.number = number;
+    this.lives = lives;
+  }
+  
+  guess(n) {
+
+    if((n === this.number) && this.lives > 0){
+      return true
+    } else if((n !== this.number) && this.lives > 0){
+      this.lives -= 1
+      return false;
+    } else throw `Already dead!`
+  }
+}
+
+// Your task is to create a method, Math.roundTo(number, precision) which rounds a given number to precision decimal places.
+
+Math.roundTo = function (number, precision) {
+  // TODO: Program Me
+  return Number(number.toFixed(precision))
+}
