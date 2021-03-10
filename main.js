57052a48e0266f5bc603250f67b8badda017f919
Mario De Los Santos <mario.jdls@gmail.com>
2021-03-10 10:28:34 -0500

    March102020Part2

--- main.js
+++ main.js
@@ -385,3 +385,60 @@ Math.roundTo = function (number, precision) {
   // TODO: Program Me
   return Number(number.toFixed(precision))
 }
+
+// The following code could use a bit of object-oriented artistry. While it's a simple method and works just fine as it is, in a larger system it's best to organize methods into classes/objects. (Or, at least, something similar depending on your language)
+// Refactor the following code so that it belongs to a Person class/object. Each Person instance will have a greet method. The Person instance should be instantiated with a name so that it no longer has to be passed into each greet method call. 
+
+class Person {
+  constructor(myName, yourName){
+    this._myName = myName
+    this.yourName = yourName
+  }
+  get name(){
+    return this._myName
+  }
+  greet(theirName){
+    this.yourName = theirName
+    return `Hello ${this.yourName}, my name is ${this.name}`
+  }
+}
+
+// Write a class Block that creates a block (Duh..)
+// ##Requirements
+// The constructor should take an array as an argument, this will contain 3 integers of the form [width, length, height] from which the Block should be created.
+// Define these methods:
+
+// `getWidth()` return the width of the `Block`
+
+// `getLength()` return the length of the `Block`
+
+// `getHeight()` return the height of the `Block`
+
+// `getVolume()` return the volume of the `Block`
+
+// `getSurfaceArea()` return the surface area of the `Block`
+
+
+class Block{
+
+  constructor(data){
+    // ...
+    this.data = data
+  }
+  getWidth(){
+    return this.data[0]
+  }
+  getLength(){
+    return this.data[1]
+  }
+  getHeight(){
+    return this.data[2]
+  }
+  getVolume(){
+    return this.data[0] * this.data[1] * this.data[2]
+  }
+  getSurfaceArea(){
+    return (2 * this.data[0] * this.data[1]) + (2 * this.data[0] * this.data[2]) + (2 * this.data[1] * this.data[2])
+  }
+  
+}
