7deaf8dcb80c718c58f97f01ecd19a22e6525fad
Mario De Los Santos <mario.jdls@gmail.com>
2021-03-10 10:55:36 -0500

    March102020part3

--- main.js
+++ main.js
@@ -442,3 +442,53 @@ class Block{
   }
   
 }
+
+
+
+// Get Coding :)
+class Animal {
+  constructor(name, age, legs, species, status) {
+    this.name = name;
+    this.age = age;
+    this.legs = legs;
+    this.species = species;
+    this.status = status;
+  }
+  introduce() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+  }
+}
+
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 0, 'shark', status);
+    
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, status){
+    super(name, age, 4, 'cat', status)
+  }
+  introduce(){
+    return `${super.introduce()}  Meow meow!`
+  }
+}
+
+class Dog extends Animal {
+   constructor(name, age, status, master){
+    super(name, age, 4, 'dog', status)
+     this.master = master
+  }
+  greetMaster(){
+    return `Hello ${this.master}`
+  }
+}
+
+// Write a function sum that accepts an unlimited number of integer arguments, and adds all of them together.
+// The function should reject any arguments that are not integers, and sum the remaining integers.
+
+function sum(...args){
+  //...
+  return args.filter(num => Number.isInteger(num)).reduce((a,b) => a + b,0)
+  }
