1a7dadf7cbc0dd74a3f4bfe25257397b8b01ab6c
Mario De Los Santos <mario.jdls@gmail.com>
2021-03-14 11:50:03 -0400

    March142020part2

--- main.js
+++ main.js
@@ -694,3 +694,16 @@ function peopleWithAgeDrink(old) {
         : old < 21 ? 'drink beer'
         : 'drink whisky'
 }
+
+// In this Kata, your function receives an array of integers as input. Your task is to determine whether the numbers are in ascending order. An array is said to be in ascending order if there are no two adjacent integers where the left integer exceeds the right integer in value.
+// For the purposes of this Kata, you may assume that all inputs are valid, i.e. non-empty arrays containing only integers.
+// Note that an array of 1 integer is automatically considered to be sorted in ascending order since all (zero) adjacent pairs of integers satisfy the condition that the left integer does not exceed the right integer in value. An empty list is considered a degenerate case and therefore will not be tested in this Kata - feel free to raise an Issue if you see such a list being tested.
+
+function inAscOrder(arr) {
+  // Code your algorithm here :)
+  if(arr.length === 1){
+    return true
+  }
+  let check = arr.map((num,i,a) => a[i] < a[i+1]).slice(0,-1)
+  return check.every(el => el === true)
+}
