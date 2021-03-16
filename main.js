8e665aa691f9090b9da8dea7f323463f18e081df
Mario De Los Santos <mario.jdls@gmail.com>
2021-03-16 13:03:56 -0400

    March 16 2021

--- main.js
+++ main.js
@@ -834,3 +834,29 @@ return str.toLowerCase().split('')
       .reverse().join('')
   
 }
+
+// Number is a palindrome if it is equal to the number with digits in reversed order. For example, 5, 44, 171, 4884 are palindromes, and 43, 194, 4773 are not.
+// Write a function which takes a positive integer and returns the number of special steps needed to obtain a palindrome. The special step is: "reverse the digits, and add to the original number". If the resulting number is not a palindrome, repeat the procedure with the sum until the resulting number is a palindrome.
+// If the input number is already a palindrome, the number of steps is 0.
+// All inputs are guaranteed to have a final palindrome which does not overflow MAX_SAFE_INTEGER.
+
+// For example, start with 87:
+
+//   87 +   78 =  165     - step 1, not a palindrome
+//  165 +  561 =  726     - step 2, not a palindrome
+//  726 +  627 = 1353     - step 3, not a palindrome
+// 1353 + 3531 = 4884     - step 4, palindrome!
+
+
+let palindromeChainLength = function(n) {
+  let step = 0
+  if(n === Number(n.toString().split('').reverse().join(''))){
+    step = 0
+  } else{
+     while(n !== Number(n.toString().split('').reverse().join(''))){
+       n += Number(n.toString().split('').reverse().join(''))
+       step++
+     }
+  }
+  return step
+};
