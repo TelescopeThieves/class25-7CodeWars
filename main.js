53843d77239d601d83ff8fc696296283bcbc384e
Mario De Los Santos <mario.jdls@gmail.com>
2021-03-23 08:59:03 -0400

    March232021

--- main.js
+++ main.js
@@ -1121,3 +1121,20 @@ function f(n){
     return result === n ? true : false ;
     
   }
+  // Write a function that returns the number of occurrences of an element in an array.
+  // Examples
+  // var arr = [0, 1, 2, 2, 3];
+  // arr.numberOfOccurrences(0) === 1;
+  // arr.numberOfOccurrences(4) === 0;
+  // arr.numberOfOccurrences(2) === 2;
+  // arr.numberOfOccurrences("a") === 0;
+
+  Array.prototype.numberOfOccurrences = function(n) {
+    return this.filter(num => num === n).length
+  }
+
+  // Write a function named repeater() that takes two arguments (a string and a number), and returns a new string where the input string is repeated that many times.
+  function repeater(string, n){
+    //Your code goes here.
+    return string.repeat(n)
+  }
