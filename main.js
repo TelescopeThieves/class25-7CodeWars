8f9fab6fc6c5ce813f4bd23cf3499bec722fc2bf
Mario De Los Santos <mario.jdls@gmail.com>
2021-03-27 12:00:23 -0400

    March272021

--- main.js
+++ main.js
@@ -1327,3 +1327,27 @@ function caffeineBuzz(n){
     return `Java`
   }else return `mocha_missing!`
   }
+
+
+  // You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+  function isValidWalk(walk) {
+    //insert brilliant code here
+    let countNS = 0
+     let countEW = 0
+     
+    if(walk.length < 10 || walk.length > 10){
+      return false
+    }
+    walk.forEach((dir) => {
+      if(dir === 'n'){
+        countNS++
+      }else if(dir === 's'){
+        countNS--
+      }else if(dir === 'e'){
+        countEW++
+      }else if(dir === 'w'){
+        countEW--
+      }
+    })
+    return countNS === 0 && countEW === 0 ? true : false;
+  }
