7be5b459adec5e357facfbdb55d8c60b8bb8bc43
Mario De Los Santos <mario.jdls@gmail.com>
2021-03-27 11:59:58 -0400

    March272021

--- main.js
+++ main.js
@@ -1299,3 +1299,31 @@ function sumOfMinimums(arr) {
   return arr.map(a => a.sort((x,y)=> x - y)).map(arr => arr[0]).reduce((acc,c) => acc + c,0)
   
 }
+
+// alternative
+function sumOfMinimums(arr) {
+  return arr.reduce((p, c) => p + Math.min(...c), 0);
+}
+
+
+// Complete the function caffeineBuzz, which takes a non-zero integer as it's one argument.
+
+// If the integer is divisible by 3, return the string "Java".
+
+// If the integer is divisible by 3 and divisible by 4, return the string "Coffee"
+
+// If the integer is one of the above and is even, add "Script" to the end of the string.
+
+// Otherwise, return the string "mocha_missing!"
+
+function caffeineBuzz(n){
+  if(n % 4 === 0 && n % 3 === 0 && n % 2 === 0){
+    return `CoffeeScript`
+  } else if (n % 4 === 0 && n % 3 === 0){
+    return `Coffee`
+  } else if(n % 3 === 0 && n % 2 === 0){
+    return `JavaScript`
+  } else if (n % 3 === 0){
+    return `Java`
+  }else return `mocha_missing!`
+  }
