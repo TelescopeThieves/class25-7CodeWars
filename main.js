d84bac33d318692de4cdc711b26fa6f7fe48e550
Mario De Los Santos <mario.jdls@gmail.com>
2021-03-27 12:12:41 -0400

    March272021 part2

--- main.js
+++ main.js
@@ -1351,3 +1351,18 @@ function caffeineBuzz(n){
     })
     return countNS === 0 && countEW === 0 ? true : false;
   }
+
+
+  // Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+  // persistence(39) === 3 // because 3*9 = 27, 2*7 = 14, 1*4=4  // and 4 has only one digit
+  
+  function persistence(num) {
+    //code me
+   let perCount = 0
+   while(num.toString().length > 1){
+    num = num.toString().split('').reduce((acc,c) => acc * Number(c),1)
+     perCount++
+   }
+   return perCount
+ }
