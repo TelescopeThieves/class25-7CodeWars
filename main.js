3789dc6de2b2c7537e186d082bec51a23757449d
Mario De Los Santos <mario.jdls@gmail.com>
2021-03-31 09:37:49 -0400

    March312021 part2

--- main.js
+++ main.js
@@ -1448,3 +1448,47 @@ function maxNumber(n){
   //your code here
   return Number(n.toString().split('').sort((x,y) => y - x).join(''))
 }
+
+
+
+// Given an array of integers , Find the maximum product obtained from multiplying 2 adjacent numbers in the array.
+function adjacentElementsProduct(array) {
+  // max product
+  let productArr = []
+  for(let i = 0; i < array.length; i++){
+    if(array[i+1]){
+      productArr.push(array[i] * array[i+1])
+    }
+    
+  }
+  return Math.max(...productArr)
+}
+
+
+// Your task is to return the sum of Triangular Numbers up-to-and-including the nth Triangular Number.
+
+// Triangular Number: "any of the series of numbers (1, 3, 6, 10, 15, etc.) obtained by continued summation of the natural numbers 1, 2, 3, 4, 5, etc."
+
+// [01]
+// 02 [03]
+// 04 05 [06]
+// 07 08 09 [10]
+// 11 12 13 14 [15]
+// 16 17 18 19 20 [21]
+
+// e.g. If 4 is given: 1 + 3 + 6 + 10 = 20.
+
+function sumTriangularNumbers(n) {
+  if(n < 0){
+    return 0;
+  }
+  let acc = 1
+  let count = 1
+  let triangle = []
+  for(let i = 1; i <= n; i++){
+    triangle.push(count)
+    acc += 1
+    count += acc
+  }
+  return triangle.reduce((a,b) => a + b, 0)
+}
