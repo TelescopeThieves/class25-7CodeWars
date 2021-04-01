8894bb4bcf8713f5b97e2ea6b6c2d6f0a1b34065
Mario De Los Santos <mario.jdls@gmail.com>
2021-04-01 09:54:01 -0400

    April12021 part2

--- main.js
+++ main.js
@@ -1550,3 +1550,28 @@ function alphabetWar(fight){
   }
    return "Let's fight again!";
 }
+
+// Complete the function that takes two numbers as input, num and nth and return the nth digit of num (counting from right to left).
+// If num is negative, ignore its sign and treat it as a positive value
+// If nth is not positive, return -1
+// Keep in mind that 42 = 00042. This means that findDigit(42, 5) would return 0
+// findDigit(5673, 4)     returns 5
+// findDigit(129, 2)      returns 2
+// findDigit(-2825, 3)    returns 8
+// findDigit(-456, 4)     returns 0
+// findDigit(0, 20)       returns 0
+// findDigit(65, 0)       returns -1
+// findDigit(24, -8)      returns -1
+
+var findDigit = function(num, nth){
+  if(nth <= 0){
+    return -1
+  }
+let arr = num.toString().split('').filter(num => num !== '-').map(num => Number(num))
+while(arr.length < nth){
+  arr.unshift(0)
+}
+arr.reverse()
+console.log(arr)
+return Math.abs(arr[nth - 1])
+}
