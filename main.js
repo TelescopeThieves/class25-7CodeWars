d28950cb0c8dea91d6a216d22127a28ce40f12ad
Mario De Los Santos <mario.jdls@gmail.com>
2021-04-03 10:21:55 -0400

    April32021

--- main.js
+++ main.js
@@ -1600,3 +1600,26 @@ var countBits = function(n) {
   let x = n.toString(2)
   return x.split('').map(Number).filter(Number).length
 };
+
+
+// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+// If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+
+function order(words){
+  // ...
+  let inOrder = []
+  if(!words){
+    return ""
+  } else{
+    words.split(' ').map((w,i,a) => {
+      let newIndex = Number(w.split('').filter(Number)[0])
+      inOrder[newIndex - 1] = w
+    })
+  }
+  return inOrder.join(' ')
+}
