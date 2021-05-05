bcffe84e18291f70be66e09b278b825968c2726a
Mario De Los Santos <mario.jdls@gmail.com>
2021-05-05 10:50:39 -0400

    may52021pt2

--- main.js
+++ main.js
@@ -2596,3 +2596,21 @@ function solve(arr){
     let diff = old - young
     return [young,old,diff]
     }
+
+
+
+    function dirReduc(arr){
+      var count = 0;
+      for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === "WEST" && arr[i+1] === "EAST" ||
+            arr[i] === "EAST" && arr[i+1] === "WEST" ||
+            arr[i] === "NORTH" && arr[i+1] === "SOUTH" ||
+            arr[i] === "SOUTH" && arr[i+1] === "NORTH") {
+            arr.splice(i, 2);
+            count++;
+            // reduce the index because the arr.length will be smaller each time
+            i--;
+        }
+      }
+      return count === 0 ? arr : dirReduc(arr);
+    }
