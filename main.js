2c1f6620cd3764b36a1eb76e24f0713e70fdcbd0
Mario De Los Santos <mario.jdls@gmail.com>
2021-05-29 16:57:47 -0400

    may292021

--- main.js
+++ main.js
@@ -3025,3 +3025,9 @@ function alan(x){
     })
     return len
   }
+
+  function whoIsPaying(name){
+    //your code here
+    if(name.length <= 2) return [name]
+    else return [name, name.slice(0,2)]
+  }
